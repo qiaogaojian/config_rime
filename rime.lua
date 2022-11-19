86868d305e2c00114c1057808da9ad002f2d36a1
qiaogaojian <qiaogaojian@vip.qq.com>
2022-11-19 15:10:23 +0800

    D:\Git\Config\config_rime Sat Nov 19 15:10:21 2022

--- rime.lua
+++ rime.lua
@@ -47,12 +47,13 @@ function xform_newline(text)
     if text == "" then 
         return "" 
     end
-
+    print("newline process"..text)
     res = string.gsub(text, "n", "_")
     return res
 end
 
 function newline_filter(input)
+    print("********************************* newline_filter *********************************")
     for candidate in input:iter() do
         candidate:get_genuine().comment = xform_newline(candidate.text)
         yield(candidate)
